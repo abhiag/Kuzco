507068bcc5299396ae7afae5c3cc1d364cc9850c
abhiag <36300569+abhiag@users.noreply.github.com>
2025-03-04 16:48:46 +0530

    Update kuzco_installer.sh

--- kuzco_installer.sh
+++ kuzco_installer.sh
@@ -40,7 +40,7 @@ echo "✅ NVIDIA GPU detected!"
 
 # Check if NVIDIA Container Toolkit is installed
 echo "🔧 Installing NVIDIA Container Toolkit..."
-yes | curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo tee /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg > /dev/null
 curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
